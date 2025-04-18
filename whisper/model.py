6f466e322f4fb5c964c605158daf633cf2844b3b
Anant Saraf <anant_saraf@brown.edu>
2025-04-18 13:52:10 -0400

    added timing.py (edits needed), minor changes to audio.py, model.py and init

--- whisper/model.py
+++ whisper/model.py
@@ -10,8 +10,10 @@ import tensorflow as tf
 # import torch.nn.functional as F
 # from torch import Tensor, nn
 
-from .decoding import decode as decode_function
+#from .decoding import decode as decode_function
 from .decoding import detect_language as detect_language_function
+from decoder import greedy_decode as decode_function
+
 from .transcribe import transcribe as transcribe_function
 
 # try:
